60e4edb83422eaf2fe42a7f411860b013b6724a1
kapumota <claraa@uni.edu.pe>
2025-01-09 14:23:27 -0500

    Update files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# coding-project-template
+## Example project
+
+```
+npm install --save gh-pages
+```
+
+```
+{
+  "name": "tu-app",
+  "version": "0.1.0",
+  "private": true,
+  "homepage": "https://kapumota.github.io/CC-3S2",
+  ...
+}
+``` 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  plugins: [react()],
+  // Ajusta la base a la subcarpeta que uses en GitHub Pages:
+//  la ruta sería '/CC-3S2/e-plantShopping/'.
+  base: '/CC-3S2/e-plantShopping/',
+  plugins: [react()]
 })
+
